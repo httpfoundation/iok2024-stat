1e4b263800d495d015c39ef0b14541e3e899b2f2
Olivér Mrakovics <oliver.mrakovics@gmail.com>
2024-03-04 14:25:47 +0100

    feat: add csv vip code upload

--- src/VipList.jsx
+++ src/VipList.jsx
@@ -1,9 +1,23 @@
+import {
+  Alert,
+  Button,
+  Divider,
+  Snackbar,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { useGetAll } from "./tools/datoCmsTools";
 import XLSXExportButton from "./XLSXExportButton";
+import { useState } from "react";
+import { SiteClient } from "datocms-client";
 
 const BASE_URL = "https://iok2024.http-alapitvany.hu/";
 
 const VipList = () => {
+  const [csv, setCsv] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
   const registrations = useGetAll("registration");
   const vipCodes = useGetAll("vipcode");
   const registeredVipUsers = registrations?.filter((reg) =>
@@ -35,17 +49,78 @@ const VipList = () => {
     ];
   };
 
+  const uploadVipList = async () => {
+    if (!csv.trim()) return;
+
+    setLoading(true);
+    const client = new SiteClient(localStorage.getItem("apiKey"));
+    const vipList = csv.split("\n").map((line) => {
+      const splitLine = line.split(";");
+      return {
+        name: splitLine[0],
+        nick_name: splitLine[1],
+        email: splitLine[2],
+      };
+    });
+
+    for (const i of vipList) {
+      try {
+        await client.items.create({
+          itemType: "94460",
+          ...i,
+        });
+      } catch {}
+    }
+
+    setLoading(false);
+    setSnackbarOpen(true);
+    setCsv("");
+  };
+
   return (
     <>
       <div>VIP kódok száma: {vipCodes?.length}</div>
       <div>Ebből regisztrált: {registeredVipUsers?.length}</div>
 
-      <XLSXExportButton
-        filename="iok2024-vip-kodok.xlsx"
-        prepareExport={prepareExport}
-      >
-        VIP kódok legenerálása és exportálása
-      </XLSXExportButton>
+      <Stack alignItems="flex-start" gap={2}>
+        <XLSXExportButton
+          filename="iok2024-vip-kodok.xlsx"
+          prepareExport={prepareExport}
+        >
+          VIP kódok legenerálása és exportálása
+        </XLSXExportButton>
+
+        <Divider flexItem sx={{ borderColor: "grey.400" }} />
+
+        <TextField
+          value={csv}
+          onChange={(e) => setCsv(e.target.value)}
+          multiline
+          rows={5}
+          label="CSV Tartalma"
+          sx={{ width: "500px" }}
+          InputLabelProps={{ shrink: true }}
+          placeholder="név;megszólítás;email"
+        />
+        <Button variant="outlined" onClick={uploadVipList} disabled={loading}>
+          VIP Lista Feltöltése
+        </Button>
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={3000}
+          onClose={() => setSnackbarOpen(false)}
+        >
+          <Alert
+            onClose={() => setSnackbarOpen(false)}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            VIP lista sikeresen feltöltve!
+          </Alert>
+        </Snackbar>
+      </Stack>
     </>
   );
 };
